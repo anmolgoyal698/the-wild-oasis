26814b7531cbd854dc04f515d2ef613bea5fdf0f
anmolgoyal698 <anmolgoyal698@gmail.com>
2025-11-28 19:38:49 +0530

    add routes and created basic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
-import styled from "styled-components";
+import Account from "./pages/Account";
+import AppLayout from "./ui/AppLayout";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
 import GlobalStyles from "./styles/GlobalStyles";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+} from "react-router-dom";
 
-const H1 = styled.h1`
-  font-size: 50px;
-  font-weight: bold;
-`;
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { index: true, element: <Navigate replace to="dashboard" /> },
+      { path: "/account", element: <Account /> },
+      { path: "/bookings", element: <Bookings /> },
+      { path: "/cabins", element: <Cabins /> },
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/settings", element: <Settings /> },
+      { path: "/users", element: <Users /> },
+    ],
+  },
+
+  { path: "/login", element: <Login /> },
+  { path: "*", element: <PageNotFound /> },
+]);
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div>
-        <H1>Hello world</H1>
-      </div>
-      ;
+      <RouterProvider router={router} />
     </>
   );
 }
