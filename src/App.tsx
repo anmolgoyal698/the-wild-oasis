72bc1b274cfd4febf8eca4abf5df99298a3d9a87
anmolgoyal698 <anmolgoyal698@gmail.com>
2025-11-28 16:11:46 +0530

    Adding global styles using styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 50px;
+  font-weight: bold;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <GlobalStyles />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <H1>Hello world</H1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      ;
     </>
-  )
+  );
 }
 
-export default App
+export default App;
