ced6598d81dc2b737062bce47fba1be771a4ecce
anmolgoyal698 <anmolgoyal698@gmail.com>
2025-12-21 14:28:12 +0530

    Implemented prefetching previous and next page data for bookings

--- src/features/bookings/useBookings.ts
+++ src/features/bookings/useBookings.ts
@@ -1,10 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
 
 export const useBookings = () => {
 
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
   const filterValue = searchParams.get("status");
   const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
@@ -27,5 +28,20 @@ export const useBookings = () => {
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  // Prefetching
+  const pageCount = Math.ceil((count || 0) / 20);
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  }
+  if(page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  }
   return { bookings, count, isPending, error };
 };
