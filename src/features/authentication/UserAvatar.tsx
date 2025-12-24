69853e513f176d9b3e82af295be6a680bad1b455
anmolgoyal698 <anmolgoyal698@gmail.com>
2025-12-24 13:48:05 +0530

    Implementing the header component

--- src/features/authentication/UserAvatar.tsx
+++ src/features/authentication/UserAvatar.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useAuth } from "./useAuth";
 
 const StyledUserAvatar = styled.div`
   display: flex;
@@ -19,3 +20,17 @@ const Avatar = styled.img`
   border-radius: 50%;
   outline: 2px solid var(--color-grey-100);
 `;
+
+
+const UserAvatar = () => {
+  const {user} = useAuth();
+  const {fullName, avatar} = user?.user_metadata || {};
+  return (
+    <StyledUserAvatar>
+      <Avatar src={avatar || "default-user.jpg"}  alt={fullName || "User Avatar"} />
+      <span>{fullName || "Unknown User"}</span>
+    </StyledUserAvatar>
+  )
+}
+
+export default UserAvatar;
